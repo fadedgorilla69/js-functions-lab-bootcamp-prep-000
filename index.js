3a124ce8e42c9e62d76c536ba97844c7ee5ddf97
Phillip Green <phillycheez11@gmail.com>
2018-01-17 00:06:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function happyHolidaysTo(name) {
   return `Happy Holidays, ${name}!`
   
 }
+
+function happyHolidayTo(holiday, name){
+   return `Happy ${holiday}, ${name}!`
+ }
+ 
+function holidayCountdown(holiday, days){
+   return `It\'s ${days} days until ${holiday}!`
+ }
